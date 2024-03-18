9f3629d6149a8c9cf09436adeee422ca8c784d46
Bousquet Clara <clarabousquet63@gmail.com>
2024-03-18 11:04:42 +0100

    side bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,9 +3,24 @@ import { dataAlbumNav, dataUserNav, imgLogo, styleIcon } from '../constants/appC
 import { RiCloseLine } from 'react-icons/ri'
 import { HiOutlineMenu } from 'react-icons/hi'
 import NavLinks from './NavLinks'
+import { useAuthContext } from '../contexts/AuthContext'
+import { useNavigate } from 'react-router-dom'
+import { FiLogOut } from 'react-icons/fi'
 
 const Sidebar = () => {
   const [mobileMenu, isMobileMenu] = useState(false);
+
+  // On récupère l'id de l'utilisateur
+  const {userId, signOut} = useAuthContext();
+
+  // On récupère le hook de navigation
+  const navigate = useNavigate();
+
+  // On crée une méthode de déconnexion
+  const handleLogout = () => {
+    signOut();
+    navigate('/');
+  }
 
   return (
     <>
@@ -13,8 +28,23 @@ const Sidebar = () => {
       <div className='hidden md:flex flex-col w-[240px] py-10 px-4 bg-black'>
         <img src={imgLogo} alt="Logo Spotify" className='w-full h-14 object-contain' />
         <NavLinks data={dataAlbumNav} marginTop={'mt-10'} />
-        <NavLinks data={dataUserNav} marginTop={'mt-5'} />
-      </div>
+        <NavLinks data={dataUserNav} marginTop={'mt-5'} userId={userId} />
+        {/* bouton de déconnexion */}
+        <div className='mt-5'>
+          <button onClick={()=>{
+            const confirmLogout = window.confirm('Voulez-vous vraiment vous déconnecter ?')
+            if(confirmLogout){
+              handleLogout();
+            }
+          }}
+          
+          className='w-full flex justify-start items-center  text-white text-sm hover:bg-green_06 font-bold p-3 '
+          
+          >
+            <FiLogOut className='w-6 h-6 mr-2' /> Se deconnecter
+          </button>
+        </div>
+              </div>
       {/* gestion des icones pour ouvrir/fermer le menu en petit ecran */}
       <div className='absolute md:hidden block top-6 right-3'>
         {mobileMenu ? (
---
+++ src/screens/OnlineScreens/Account/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Account = () => {
+  return (
+    <div className='text-3xl text-bold '> Mon compte</div>
+  )
+}
+
+export default Account
